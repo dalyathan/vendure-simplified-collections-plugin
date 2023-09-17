57cf0efa9c1013e55b458902ca0b893a89cade0c
Surafel Melese <surafelmelese09@gmail.com>
2023-09-17 21:32:51 +0300

    feat: added more language translations for the work 'Collection'. Updated tsconfig to properly generate .dist

--- src/plugin.ts
+++ src/plugin.ts
@@ -17,7 +17,22 @@ import { CollectionFiltersUpdateService } from './api/collection-filters-update.
         type: "text",
         defaultValue: "",
         ui: { component: "product-collections" },
-        label: [{ languageCode: LanguageCode.en, value: "Collections" }],
+        label: [
+          { languageCode: LanguageCode.en, value: "Collections" },
+          {languageCode: LanguageCode.pl, value: 'Kolekcje'},
+          { languageCode: LanguageCode.it, value: "Collezioni" },
+          {languageCode: LanguageCode.de, value: 'Sammlungen'},
+          { languageCode: LanguageCode.es, value: "Colecciones" },
+          {languageCode: LanguageCode.ar, value: 'مجموعات'},
+          { languageCode: LanguageCode.pt_BR, value: "Categorias" },
+          {languageCode: LanguageCode.he, value: "םיפסוא"},
+          { languageCode: LanguageCode.uk, value: "Колекції" },
+          {languageCode: LanguageCode.zh_Hans, value: '商品系列'},
+          { languageCode: LanguageCode.ru, value: "Коллекции" },
+          {languageCode: LanguageCode.cs, value: "Kolekce"},
+          { languageCode: LanguageCode.pt_PT, value: "Categorias" },
+          {languageCode: LanguageCode.zh_Hant, value: '商品系列'},
+    ],
       });
     return config;
   }
